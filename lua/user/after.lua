95610fdbf56e734ea64076496338b84f144e9dd3
Tomas Apodaca <45415133+tomas-nava@users.noreply.github.com>
2022-12-28 23:32:36 -0800

    using luan/nvim now instead of luan/vimfiles

--- lua/user/after.lua
+++ lua/user/after.lua
@@ -13,6 +13,9 @@ vim.keymap.set('n', '<C-k>', '<C-w>k', { silent = true })
 vim.keymap.set('n', '<C-h>', '<C-w>h', { silent = true })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { silent = true })
 
+-- show count of word under the cursor
+vim.keymap.set('n', '<Leader>yc', 'yiw:%s/<C-r>0//gn<CR>', { silent = true })
+
 -- highlight yanked text
 vim.api.nvim_exec([[
   augroup YankHighlight
@@ -23,3 +26,8 @@ vim.api.nvim_exec([[
 
 -- prefer gigavolt colorscheme
 vim.cmd[[colorscheme base16-gigavolt]]
+
+-- run oi with the word under the cursor
+vim.keymap.set('n', '<Leader>yoi', 'yiw:!oi<Space><C-r>0<CR>', { silent = true })
+-- run oi with the word under the cursor, specifying a commit
+vim.keymap.set('n', '<Leader>yoc', 'yiw:!oi<Space>-c<Space><C-r>0<CR>', { silent = true })
