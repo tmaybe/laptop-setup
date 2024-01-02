be54170b6c8893a1851d7f08c8ec0eb55ea8350d
Tomas Apodaca <546665+tmaybe@users.noreply.github.com>
2024-01-02 10:55:34 -0800

    this doesn't work anymore

--- init.lua
+++ init.lua
@@ -1,24 +1,5 @@
 
 --
--- HAMMERSPOON CONFIG MANAGEMENT
---
-
--- reload the config when this file changes
-local function reloadConfig(files)
-    local doReload = false
-    for _, file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
-    end
-    if doReload then
-        hs.reload()
-    end
-end
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-hs.alert.show("Hammerspoon config reloaded")
-
---
 -- WINDOW MANAGEMENT
 --
 
